de31be3d3cc8a0b9f54e1bf3dcb417e4bea7f910
Praveen <praveen.s@sap.com>
2015-05-26 16:03:40 +0530

    First angular app

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-require(["app"], function(App){
-	var app = new App();
-	app.create("login");
+require.config({
+
+		paths: {
+			'jquery': '../tp/jquery',
+			'angular': '../tp/angular.min',
+			'loginModule': 'login/loginModule',
+			'app':'app'
+		},
+
+		shim: { 
+			'app' :{
+				deps:['angular', 'loginModule']
+			},
+			'loginModule' : {
+				deps : ['angular']
+			}
+		}
+});
+
+require(["app"], function(){
+
+	angular.bootstrap(document, ['app']);
 });
