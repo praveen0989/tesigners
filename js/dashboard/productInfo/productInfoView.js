e4890999a2c5b72a6ae398b24aed2a00495d137e
Amit Kumar Singh <amit07sun@gmail.com>
2015-06-02 23:47:49 +0530

    productInfoView CSS changes

--- js/dashboard/productInfo/productInfoView.js
+++ js/dashboard/productInfo/productInfoView.js
@@ -7,16 +7,16 @@ define([], function(){
 	this.password;
 	this.keepMeLoggedIn;
 	this. phoneNumber;
-	
+
 	this.isRendered = false;
-	
+
 	this.registerEvents = function(controller){
 		var that = this;
 		$("#submitButton").click(function(){
 			controller.onLogin($("#usernameinput").val(), $("#passwordinput").val());
 		});
 	};
-	
+
 	var populateOptions = function(array){
 		var i = 0;
 		var div = "";
@@ -24,43 +24,44 @@ define([], function(){
 			var category = array[i];
 			div = div + "<option id=" +category.id +">"+ category.name +"</option>";
 		}
-		div = div + "</select></span>";
+		div = div + "</select></div>";
 		return div;
 	};
-	
-	
+
+
 	this.render = function(container){
-		
+
 		if(!this.isRendered){
 			var divClose = "</div>";
-			
-			var categorySelectDiv = "<div id='categorySelectDiv'><span><select name='categorySelect'>";
-			categorySelectDiv += populateOptions(this.model.categories) + "</div>";
-			
-			var subcategoryDiv = "<div id='categorySelectDiv'>";
-			
-			var tShirtCategories = "<span> <select name='shirtType'>";
+
+			var categorySelectDiv = "<div class='form-horizontal'><div id='categorySelectDiv' class='form-group'><div class='col-md-2'><label name='categorySelectLabel'>Please select your category</label></div><div class='col-md-2'><select name='categorySelect' class='form-control'>";
+			categorySelectDiv += populateOptions(this.model.categories) + "</div></div>";
+
+			var subcategoryDiv = "<div class='form-horizontal'><div id='subCategorySelectDiv' class='form-group'>";
+			var subcategoryLabel = "<div class='col-md-2'><label name='categorySelectLabel'>Please select your sub-category</label></div>";
+
+			var tShirtCategories = "<div class='col-md-2'> <select name='shirtType' class='form-control'>";
 			tShirtCategories+= populateOptions(this.model.tShirtCategories);
-			
-			var tShirtFabricsCategories = "<span> <select name='fabricType'>";
+
+			var tShirtFabricsCategories = "<div class='col-md-2'> <select name='fabricType' class='form-control'>";
 			tShirtFabricsCategories+= populateOptions(this.model.tshirtFabrics);
-			
-			var tshirtSleevesCategories = "<span> <select name='tshirtSleevesType'>";
+
+			var tshirtSleevesCategories = "<div class='col-md-2'> <select name='tshirtSleevesType' class='form-control'>";
 			tshirtSleevesCategories+= populateOptions(this.model.tshirtSleeves);
-			
-			var tshirtGsmCategories = "<span> <select name='tshirtGsmType'>";
+
+			var tshirtGsmCategories = "<div class='col-md-2'> <select name='tshirtGsmType' class='form-control'>";
 			tshirtGsmCategories+= populateOptions(this.model.tshirtGsm);
-			
-			var addButton = "<span><button name='addButton' id='addButton' type='button'>Add</button></span>";
-			
-			
-			
-			
-			
-			container.append( categorySelectDiv + divClose + subcategoryDiv + tShirtCategories + tShirtFabricsCategories + tshirtSleevesCategories + tshirtGsmCategories + addButton + divClose);
+
+			var addButton = "<div class='col-md-1'><button name='addButton' id='addButton' type='button' class='form-control btn btn-primary'>Add</button></div></div>	";
+
+
+
+
+
+			container.append( categorySelectDiv + divClose + subcategoryDiv + subcategoryLabel + tShirtCategories + tShirtFabricsCategories + tshirtSleevesCategories + tshirtGsmCategories + addButton + divClose);
 			this.registerEvents(controller);
 		}
-		
+
 		$("#productDiv").removeClass("dashboardItemHidden");
 		$("#productDiv").addClass("dashboardItemVisible");
 	};
