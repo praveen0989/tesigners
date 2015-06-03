9b0e9d4cdba020179420aa8476a9d78d82fcf7e0
Praveen <praveen.s@sap.com>
2015-06-03 17:56:47 +0530

    Product view changes css

--- js/dashboard/productInfo/productInfoView.js
+++ js/dashboard/productInfo/productInfoView.js
@@ -57,22 +57,22 @@ define(["jqGrid"], function(JQGrid){
 		if(!this.isRendered){
 			var divClose = "</div>";
 
-			var categorySelectDiv = "<div id='categorySelectDiv'><span><select id='categorySelect' name='categorySelect'>";
+			var categorySelectDiv = "<div class='form-horizontal'><div id='categorySelectDiv' class='form-group'><div class='col-md-2'><label name='categorySelectLabel'>Please select your category</label></div><div class='col-md-2'><select id='categorySelect' name='categorySelect' class='form-control'>";
 			categorySelectDiv += populateOptions(this.model.categories) + "</div></div>";
 
 			var subcategoryDiv = "<div class='form-horizontal'><div id='subCategorySelectDiv' class='form-group'>";
 			var subcategoryLabel = "<div class='col-md-2'><label name='categorySelectLabel'>Please select your sub-category</label></div>";
 
-			var tShirtCategories = "<span> <select name='shirtType' id='shirtType'>";
+			var tShirtCategories = "<div class='col-md-2'> <select name='shirtType' class='form-control' id='shirtType'>";
 			tShirtCategories+= populateOptions(this.model.tShirtCategories);
 
-			var tShirtFabricsCategories = "<span> <select name='fabricType' id='fabricType'>";
+			var tShirtFabricsCategories = "<div class='col-md-2'> <select name='fabricType' id='fabricType' class='form-control'>";
 			tShirtFabricsCategories+= populateOptions(this.model.tshirtFabrics);
 
-			var tshirtSleevesCategories = "<span> <select name='tshirtSleevesType' id='tshirtSleevesType'>";
+			var tshirtSleevesCategories = "<div class='col-md-2'> <select name='tshirtSleevesType' class='form-control' id='tshirtSleevesType'>";
 			tshirtSleevesCategories+= populateOptions(this.model.tshirtSleeves);
 
-			var tshirtGsmCategories = "<span> <select name='tshirtGsmType' id='tshirtGsmType'>";
+			var tshirtGsmCategories = "<div class='col-md-2'> <select name='tshirtGsmType' class='form-control' id='tshirtGsmType'>";
 			tshirtGsmCategories+= populateOptions(this.model.tshirtGsm);
 
 			var addButton = "<div class='col-md-1'><button name='addButton' id='addButton' type='button' class='form-control btn btn-primary'>Add</button></div></div>	";
@@ -86,19 +86,18 @@ define(["jqGrid"], function(JQGrid){
 			$("#productsTable").jqGrid({
                 datatype: "local",
                 data: this.model.supportedEntities,
-                colNames:['T Shirt type','Fabric type', 'Sleeve','GSM'],
+                colNames:['T Shirt type','Fabric type', 'Sleeve','GSM', 'Remove'],
                 colModel:[
-                    {name:'T Shirt type',index:'T-Shirt Type',width:100},
-                    {name:'Fabric Type',index:'Fabric Type', width:100},
-                    {name:'Sleeve',index:'Sleeve', width:100},
-                    {name:'GSM',index:'GSM', width:100}
+                    {name:'T Shirt type',index:'T-Shirt Type'},
+                    {name:'Fabric Type',index:'Fabric Type'},
+                    {name:'Sleeve',index:'Sleeve'},
+                    {name:'GSM',index:'GSM'},
+					{name : "Remove", index : 'Remove'}
                 ],
-                loadonce: false,
+            
                 sortname: 'T-Shirt Type',
                 sortorder: "asc",
-                height: "auto", //210,
-                width:600,
-                viewrecords: true
+                height: "auto"
             });
 
 			this.registerEvents(controller);
