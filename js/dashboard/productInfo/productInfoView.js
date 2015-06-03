83069ceaf0af03e859aa7f49c6ac24067269f560
Amit Kumar Singh <amit07sun@gmail.com>
2015-06-03 21:09:47 +0530

    creating order model

--- js/dashboard/productInfo/productInfoView.js
+++ js/dashboard/productInfo/productInfoView.js
@@ -14,18 +14,18 @@ define(["jqGrid"], function(JQGrid){
 		var that = this;
 		$("#addButton").click(function(){
 
-			
+
 			var tShirtType = $('#shirtType').val();
 			var fabricType = $('#fabricType').val();
 			var sleeveType = $('#tshirtSleevesType').val();
 			var gsmType = $('#tshirtGsmType').val();
-			
+
 			var tShirtId = $("#shirtType")[0]["selectedOptions"][0].id;
 			var fabricId = $('#fabricType')[0]["selectedOptions"][0].id;
 			var sleeveId = $('#tshirtSleevesType')[0]["selectedOptions"][0].id;
 			var gsmId = $('#tshirtGsmType')[0]["selectedOptions"][0].id;
 			var categoryId = $("#categorySelect")[0]["selectedOptions"][0].id;
-			
+
 			that.model.supportedIds.push(tShirtId + fabricId + sleeveId + gsmId + categoryId);
 			var obj = {
 						"T Shirt type" : tShirtType,
@@ -58,7 +58,7 @@ define(["jqGrid"], function(JQGrid){
 		$('#metricsDiv').css('display','none');
 		$('#accountsDiv').css('display','none');
 		$('#productDiv').css('display','block');
-	}
+	};
 
 	this.render = function(container){
 		showProductInfoPage();
@@ -103,7 +103,7 @@ define(["jqGrid"], function(JQGrid){
                     {name:'GSM',index:'GSM'},
 					{name : "Remove", index : 'Remove'}
                 ],
-            
+
                 sortname: 'T-Shirt Type',
                 sortorder: "asc",
                 height: "auto"
